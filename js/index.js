6c156235ae91aa97dc3a69d60aee92b0816846c4
hardiksingla <07hardiksingla@gmail.com>
2023-08-12 21:53:07 +0530

    fixed about us scroll issue

--- js/index.js
+++ js/index.js
@@ -33,20 +33,7 @@ videocardContainers.forEach((item, i) => {
   });
 });
 
-var body = document.querySelector("body");
-body.style.overflowY = "hidden"
 
-function togglediv() {
-  var div = document.getElementById("top-destination");
-  var div1 = document.getElementById("navbar");
-  var div2 = document.getElementById("footer");
-  var div3 = document.getElementById("copyright");
-  div.style.display = div.style.display == "block" ? "none" : "block";
-  div1.style.display = div1.style.display == "flex" ? "none" : "flex";
-  div2.style.display = div2.style.display == "flex" ? "none" : "flex";
-  div3.style.display = div3.style.display == "block" ? "none" : "block";
-  body.style.overflowY = body.style.overflowY == "hidden" ? "visible" : "hidden";
-}
 
 var tryNowButton = document.getElementById("try-now-button");
 const popup = document.getElementById("popup");
